8cf07d419f6726ac316046b7b741bf29916bc129
julien-jungo <julienjungo+github.com@pm.me>
2023-09-25 07:27:47 +0200

    #3 Add minimal text to speech example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  public text = 'Hello World';
+
+  private synthesis?: SpeechSynthesis;
+  private voice?: SpeechSynthesisVoice;
+
+  ngOnInit() {
+    this.synthesis = window.speechSynthesis;
+    this.voice = this.synthesis.getVoices()
+      .filter(voice => voice.lang === 'en')[0];
+  }
+
+  speak() {
+    const utterance = new SpeechSynthesisUtterance(this.text);
+
+    utterance.voice = this.voice!;
+
+    this.synthesis?.speak(utterance);
+  }
+
+}
