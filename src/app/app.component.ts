c40147fc2f025d4df3d3bb49776313f0c344303a
julien-jungo <julienjungo+github.com@pm.me>
2023-10-04 08:47:12 +0200

    #3 Use separate component for TTS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SpeechService } from "./services/speech.service";
 
 @Component({
   selector: 'app-root',
@@ -7,13 +6,5 @@ import { SpeechService } from "./services/speech.service";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  public text = 'Hallo Welt';
-
-  constructor(private readonly speechService: SpeechService) {}
-
-  public speak(): void {
-    this.speechService.speak({ text: this.text, lang: 'de-DE' });
-  }
 
 }
--- src/app/views/hello-world/hello-world.component.ts
+++ src/app/views/hello-world/hello-world.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SpeechService } from "../../services/speech.service";
 
 @Component({
   selector: 'app-hello-world',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./hello-world.component.scss']
 })
 export class HelloWorldComponent {
+
+  public text = 'Hallo Welt';
+
+  constructor(private readonly speechService: SpeechService) {}
+
+  public speak(): void {
+    this.speechService.speak({ text: this.text, lang: 'de-DE' });
+  }
 
 }
